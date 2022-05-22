731e2716d242880e9b67748c2bbccf070836adaa
Rahim Mahat <rahimmahat99@gmail.com>
2022-05-22 19:39:41 +0530

    leetcode add binary

---
+++ add_binary.py
@@ -0,0 +1,28 @@
+'''
+Given two binary strings a and b, return their sum as a binary string.
+'''
+
+def addBinary(a: str, b: str) -> str:
+    result = []
+    carry = 0
+    i, j = len(a)-1, len(b)-1
+
+    while i>=0 or j>=0 or carry:
+        total = carry
+
+        if i >= 0:
+            total += int(a[i])
+            i -= 1
+        if j >= 0:
+            total += int(b[j])
+            j -= 1
+
+        result.append(str(total%2))
+        carry = total // 2
+
+    return ''.join(reversed(result))
+
+a = "1010"
+b = "1011"
+res = addBinary(a, b)
+print(res)
